83a8b12e9b4be12ccf9a5c633a322ed5e0b8c076
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-02 11:27:42 +0600

    Added Linked List counter Function

--- linkedListPractice.cpp
+++ linkedListPractice.cpp
@@ -33,6 +33,19 @@ void display(Node *n)
     cout << endl
          << endl;
 }
+
+int countLength(Node *&head)
+{
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->Next;
+    }
+    return count;
+}
+
 int main()
 {
     Node *head = NULL;
